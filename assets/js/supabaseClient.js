e97740f9a8680aa2d2aaf3773e295d3eb952132f
jdbunkers-ui <j.d.bunkers@gmail.com>
2026-01-24 13:44:01 -0500

    Update supabaseClient.js

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -1,4 +1,6 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+/*import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";*/
+
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.5/+esm";
 
 /**
  * IMPORTANT:
